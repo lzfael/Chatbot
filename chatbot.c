c76a43a22e387f36f300b15054e3e4d927924b2a
Rafael Luiz <luizrafaelcontato@gmail.com>
2025-04-28 19:40:07 -0300

    feat: criação da função ExibirMenu

--- chatbot.c
+++ chatbot.c
@@ -3,43 +3,28 @@
 void Consulta();
 void InformacoesPlanoSaude();
 void EspecialidadesMedicasDisponiveis();
+void ExibirMenu();
 
 int main() {
-    int opcao = 0;
-
+    printf("----------------------------------------\n");
     printf("Seja bem-vindo a Clinica do C!\n");
-    printf("Como posso ajudar voce hoje?\n");
-    printf("1. Agendar Consulta\n");
-    printf("2. Informacoes sobre nosso plano de saude\n");
-    printf("3. Ver especialidades medicas disponiveis\n");
-    printf("Escolha uma opcao: ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
-        case 1:
-            Consulta();
-            break;
-        case 2:
-            InformacoesPlanoSaude();
-            break;
-        case 3:
-            EspecialidadesMedicasDisponiveis();
-            break;
-        default:
-            printf("\nOpcao invalida!\n");
-            break;
-    }
+    printf("----------------------------------------\n");
+    ExibirMenu();
+    
 
     return 0;
 }
 
 void Consulta() {
-    char nome[50], especialidade[50], data[11], horario[6];
+    char firstName[100], lastName[100], especialidade[50], data[11], horario[6];
 
     printf("\nOla! Vamos agendar sua consulta.\n");
 
-    printf("Qual seu nome completo? ");
-    scanf("%s", nome);
+    printf("Qual seu primeiro nome? ");
+    scanf("%s", firstName);
+
+    printf("Qual seu ultimo nome? ");
+    scanf("%s", lastName);
 
     printf("Qual especialidade medica voce precisa? ");
     scanf("%s", especialidade);
@@ -51,7 +36,7 @@ void Consulta() {
     scanf("%s", horario);
 
     printf("Obrigado! Sua consulta foi agendada:\n");
-    printf("- Nome: %s \n", nome);
+    printf("- Nome: %s %s\n", firstName, lastName);
     printf("- Especialidade: %s \n", especialidade);
     printf("- Data: %s \n", data);
     printf("- Horario: %s \n", horario);
@@ -76,4 +61,33 @@ void EspecialidadesMedicasDisponiveis(){
     printf("- Ortopedia\n");
     printf("- Pediatria\n");
     printf("- Ginecologia\n");
+}
+
+void ExibirMenu() {
+    int opcao = 0;
+    printf("\nComo posso ajudar voce hoje?\n");
+    printf("1. Agendar Consulta\n");
+    printf("2. Informacoes sobre nosso plano de saude\n");
+    printf("3. Ver especialidades medicas disponiveis\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            Consulta();
+            ExibirMenu();
+            break;
+        case 2:
+            InformacoesPlanoSaude();
+            ExibirMenu();
+            break;
+        case 3:
+            EspecialidadesMedicasDisponiveis();
+            ExibirMenu();
+            break;
+        default:
+            printf("\nOpcao invalida! \n Tente Novamente:\n");
+            ExibirMenu();
+            break;
+    }
 }
