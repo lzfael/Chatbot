fe01867195681f65d7a0ce698d0ef1d1b96e3406
Rafael Luiz <luizrafaelcontato@gmail.com>
2025-04-28 18:59:22 -0300

    feat: criação da função Consulta

---
+++ chatbot.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// Declaração da função Consulta
+void Consulta();
+
+int main() {
+    int opcao = 0;
+
+    printf("Seja bem-vindo a Clinica do C!\n");
+    printf("Como posso ajudar voce hoje?\n");
+    printf("1. Agendar Consulta\n");
+    printf("2. Informacoes sobre nosso plano de saude\n");
+    printf("3. Ver especialidades medicas disponiveis\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            Consulta();
+            break;
+        case 2:
+            printf("Voce escolheu informacoes sobre nosso plano de saude.\n");
+            break;
+        case 3:
+            printf("Voce escolheu ver especialidades medicas disponiveis.\n");
+            break;
+        default:
+            printf("Opcao invalida. Tente novamente.\n");
+            break;
+    }
+
+    return 0;
+}
+
+void Consulta() {
+    char nome[50], especialidade[50], data[11], horario[6];
+
+    printf("Ola! Vamos agendar sua consulta.\n");
+
+    printf("Qual seu nome completo? ");
+    scanf("%s", nome);
+
+    printf("Qual especialidade medica voce precisa? ");
+    scanf("%s", especialidade);
+
+    printf("Para qual dia voce gostaria de agendar? (DD/MM/AAAA) ");
+    scanf("%s", data);
+
+    printf("Qual o melhor horario para voce? (HH:MM) ");
+    scanf("%s", horario);
+
+    printf("Obrigado! Sua consulta foi agendada:\n");
+    printf("- Nome: %s \n", nome);
+    printf("- Especialidade: %s \n", especialidade);
+    printf("- Data: %s \n", data);
+    printf("- Horario: %s \n", horario);
+}
